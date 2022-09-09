e3d17c763c8cffe42e7d513214b69e4e91912cc5
Felipe Salomao <lipesalomaoclaf@gmail.com>
2022-09-09 11:06:48 -0300

    feat: added api link

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 const { "nextAuth.token": token } = parseCookies();
 export const api = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://dashboards-server.herokuapp.com/",
   headers: {
     Authorization: `${token}`,
     "Content-Type": "application/json",
-  }
+  },
 });
