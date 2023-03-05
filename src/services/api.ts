9e432f7ae884d08220305d940a916b282ef8fba8
Felipe Salomao <lipesalomaoclaf@gmail.com>
2023-03-05 10:58:46 -0300

    feat: added variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 const { "nextAuth.token": token } = parseCookies();
 export const api = axios.create({
-  baseURL: "http://localhost:3001/",
   // baseURL: "http://localhost:3001/",
+  baseURL: "dashboardserver-production.up.railway.app/",
 
   headers: {
     Authorization: `${token}`,
